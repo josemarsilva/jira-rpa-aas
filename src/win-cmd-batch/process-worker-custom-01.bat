e2b9898c924bc8cb61672305059405fd1cc83a39
josemarsilva <josemar.furegatti@gmail.com>
2020-04-17 11:05:56 -0300

    process-worker-custom-01.bat

---
+++ src/win-cmd-batch/process-worker-custom-01.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+SET BASE_PATH=C:\GitHome\ws-github-01\descaract-dados
+ECHO.
+ECHO CD %BASE_PATH%\src\sql-custom-01
+ECHO.
+CD %BASE_PATH%\src\sql-custom-01
+ECHO.
+ECHO CALL run_all_in_one_db2.bat
+ECHO.
+CALL run_all_in_one_db2.bat
--- src/win-cmd-batch/process-worker.bat
+++ src/win-cmd-batch/process-worker.bat
@@ -6,8 +6,9 @@ ECHO   Arg's: %1 %2 %3 %4 %5 %6 %7 %8
 ECHO   Data : %DATE%
 ECHO   Hora : %TIME%
 ECHO.
-ECHO ^<status^>SUCESSO^</status^>
-ECHO ^<package-zip^>C:\GitHome\ws-github-01\jira-rpa-aas\src\powershell\process-worker.tmp^</package-zip^>
+ECHO ^<status^>FAIL^</status^>
+ECHO ^<status-message^>Falha^</status-message^>
+ECHO ^<package-zip^>C:\GitHome\ws-github-01\jira-rpa-aas\src\powershell\rpa-aas-process.log^</package-zip^>
 ECHO.
 ECHO Finishing 'process-worker.bat'.
 ECHO.
