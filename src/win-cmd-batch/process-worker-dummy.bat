8a2260748db3ea43f6ac839aade07e8f6ea32e01
Josemar Silva <josemarsilva@yahoo.com.br>
2021-06-04 20:24:38 -0300

    src/win-cmd-batch/process-worker-dummy.bat

---
+++ src/win-cmd-batch/process-worker-dummy.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+ECHO.
+ECHO Starting 'process-worker.bat' ...
+ECHO.
+ECHO   Arg's: %1 %2 %3 %4 %5 %6 %7 %8
+ECHO   IssueId : %1
+ECHO   IssueKey: %2
+ECHO   Data    : %DATE%
+ECHO   Hora    : %TIME%
+ECHO.
+ECHO ^<status^>FAIL^</status^>
+ECHO ^<status-message^>Falha^</status-message^>
+ECHO ^<package-zip^>C:\GitHome\ws-github-01\jira-rpa-aas\src\powershell\rpa-aas-process.log^</package-zip^>
+ECHO.
+ECHO Finishing 'process-worker.bat'.
+ECHO.
